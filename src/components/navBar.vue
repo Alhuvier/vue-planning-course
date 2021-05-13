0411a16082ce3629b9a398d345df69cf98b474a9
Alrick Huvier <al.huvier@live.fr>
2021-05-13 17:04:26 +0200

    add vuetify nav bar

---
+++ src/components/navBar.vue
@@ -0,0 +1,31 @@
+<template>
+<footer>
+  <v-bottom-navigation v-model="value">
+    <v-btn value="recent">
+      <span>Recent</span>
+
+      <v-icon>mdi-history</v-icon>
+    </v-btn>
+
+    <v-btn value="favorites">
+      <span>Favorites</span>
+
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn value="nearby">
+      <span>Nearby</span>
+
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</footer>
+</template>
+
+<script lang="ts">
+
+export default {
+  data: () :{value:string} => ({ value: 'recent' })
+}
+
+</script>

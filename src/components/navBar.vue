ffd72c08d1a6d8a432cdd99813e64233f9907d66
Alrick Huvier <al.huvier@live.fr>
2021-05-15 16:56:25 +0200

    implements router

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,31 +1,23 @@
 <template>
-<footer>
   <v-bottom-navigation v-model="value">
-    <v-btn value="recent">
-      <span>Recent</span>
+    <v-btn value="planning">
+      <span>Planning</span>
 
-      <v-icon>mdi-history</v-icon>
+      <v-icon>mdi-calendar</v-icon>
     </v-btn>
 
-    <v-btn value="favorites">
-      <span>Favorites</span>
+    <v-btn value="courses">
+      <span>Courses</span>
 
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
-
-    <v-btn value="nearby">
-      <span>Nearby</span>
-
-      <v-icon>mdi-map-marker</v-icon>
+      <v-icon>mdi-basket</v-icon>
     </v-btn>
   </v-bottom-navigation>
-</footer>
 </template>
 
 <script lang="ts">
 
 export default {
-  data: () :{value:string} => ({ value: 'recent' })
+  data: () :{value:string} => ({ value: 'planning' })
 }
 
 </script>
---
+++ src/views/Planning.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="planning">
+    <h1>This is a planning page</h1>
+  </div>
+</template>

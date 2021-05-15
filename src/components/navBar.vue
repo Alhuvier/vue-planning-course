49d52cce6a0ea9f435bab98a16980b7cba5abb4d
Alrick Huvier <al.huvier@live.fr>
2021-05-15 17:11:56 +0200

    add link to vbutton

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,15 +1,23 @@
 <template>
   <v-bottom-navigation v-model="value">
-    <v-btn value="planning">
-      <span>Planning</span>
+    <v-btn value="home" to="/">
+      <span>Home</span>
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
 
+    <v-btn value="planning" to="/planning">
+      <span>Planning</span>
       <v-icon>mdi-calendar</v-icon>
     </v-btn>
 
-    <v-btn value="courses">
+    <v-btn value="courses" to="/courses">
       <span>Courses</span>
-
       <v-icon>mdi-basket</v-icon>
+    </v-btn>
+
+    <v-btn value="about" to="/about">
+      <span>About</span>
+      <v-icon>mdi-information-outline</v-icon>
     </v-btn>
   </v-bottom-navigation>
 </template>
